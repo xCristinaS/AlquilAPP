cc842742b3a5d5c306b7416fb2800499e7c2f7e0
Alejandro Torres <alejandro123888@hotmail.com>
2016-03-18 20:44:41 +0100

    Comprobando cosas

--- proyecto/app/src/main/java/c/proyecto/MainActivity.java
+++ proyecto/app/src/main/java/c/proyecto/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        System.out.println("aaaaaaaaa");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
