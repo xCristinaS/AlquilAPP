363621ea2ac1ce445dedfad0d3592235801cd3c5
Alejandro Torres <alejandro123888@hotmail.com>
2016-03-20 19:11:46 +0100

    Datos simples cargados, a falta de recycler view

---
+++ proyecto/app/src/main/java/c/proyecto/Constantes.java
@@ -0,0 +1,14 @@
+package c.proyecto;
+
+
+public final class Constantes {
+    public static final String MONEDA = "€";
+    public static final String UNIDAD = "m²";
+
+    public static final String CASA = "Casa";
+    public static final String PISO = "Piso";
+    public static final String HABITACION = "Habitación";
+    public static final String CAMAS = "Camas";
+
+}
+
