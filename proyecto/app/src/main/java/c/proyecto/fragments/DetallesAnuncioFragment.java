87c7272f331832d9526bd77acf34d52da66c7fc9
Alejandro Torres <alejandro123888@hotmail.com>
2016-03-20 18:11:22 +0100

    Creando fragmento Detalles anuncio

--- proyecto/app/src/main/java/c/proyecto/fragments/DetallesAnuncioFragment.java
+++ proyecto/app/src/main/java/c/proyecto/fragments/DetallesAnuncioFragment.java
@@ -1,17 +1,50 @@
 package c.proyecto.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import c.proyecto.R;
+import c.proyecto.models.Anuncio;
 
 
 public class DetallesAnuncioFragment extends Fragment {
 
+    private static final String ARG_ANUNCIO = "anuncio";
+    private Anuncio mAnuncio;
+    private ImageView imgFoto;
+    private ImageView imgAvatar;
+    private TextView lblNombre;
+    private TextView lblPrecio;
+    private TextView lblTamano;
+    private ImageView imgTipoVivienda;
+    private TextView lblTipoVivienda;
+    private ImageView imgCamas;
+    private TextView lblCamas;
+    private TextView lblNumCamas;
+    private TextView lblNumHuespedes;
+    private RecyclerView rvPrestaciones;
+    private TextView lblDescripcionNoDisponible;
+    private TextView lblDescripcion;
+
+
+    public static DetallesAnuncioFragment newInstance(Anuncio anuncio) {
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_ANUNCIO, anuncio);
+
+        DetallesAnuncioFragment fragment = new DetallesAnuncioFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -21,5 +54,35 @@ public class DetallesAnuncioFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        initViews();
+        mAnuncio = getArguments().getParcelable(ARG_ANUNCIO);
+
+        //Comprobar si es mi anuncio.
+
+        mostrarDatos();
+
+    }
+
+    private void initViews() {
+        Activity act = getActivity();
+
+        imgFoto = (ImageView) act.findViewById(R.id.imgFoto);
+        imgAvatar = (ImageView) act.findViewById(R.id.imgAvatar);
+        lblNombre = (TextView) act.findViewById(R.id.lblNombre);
+        lblPrecio = (TextView) act.findViewById(R.id.lblPrecio);
+        lblTamano = (TextView) act.findViewById(R.id.lblTamano);
+        imgTipoVivienda = (ImageView) act.findViewById(R.id.imgTipoVivienda);
+        lblTipoVivienda = (TextView) act.findViewById(R.id.lblTipoVivienda);
+        imgCamas = (ImageView) act.findViewById(R.id.imgCamas);
+        lblCamas = (TextView) act.findViewById(R.id.lblCamas);
+        lblNumCamas = (TextView) act.findViewById(R.id.lblNumCamas);
+        lblNumHuespedes = (TextView) act.findViewById(R.id.lblNumero);
+        rvPrestaciones = (RecyclerView) act.findViewById(R.id.rvPrestaciones);
+        lblDescripcionNoDisponible = (TextView) act.findViewById(R.id.lblDescripcionNoDisponible);
+        lblDescripcion = (TextView) act.findViewById(R.id.lblDescripcion);
+    }
+
+    private void mostrarDatos() {
+        Picasso.with(getActivity()).load(mAnuncio.)
     }
 }
---
+++ proyecto/app/src/main/java/c/proyecto/models/Anuncio.java
@@ -0,0 +1,19 @@
+package c.proyecto.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by aleja on 20/03/2016.
+ */
+public class Anuncio implements Parcelable {
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
+}
