ce28ca13d9d15c426bc5530428a2a6794a9ea67b
Alejandro Torres <alejandro123888@hotmail.com>
2016-03-21 21:14:30 +0100

    Layout Registro finiquitado

--- proyecto/app/src/main/java/c/proyecto/activities/RegistroActivity.java
+++ proyecto/app/src/main/java/c/proyecto/activities/RegistroActivity.java
@@ -2,14 +2,30 @@ package c.proyecto.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 import c.proyecto.R;
 
 public class RegistroActivity extends AppCompatActivity {
 
+    private TextView lblCancelar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        initViews();
+    }
+
+    private void initViews() {
+        lblCancelar = (TextView) findViewById(R.id.lblCancelar);
+        lblCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
