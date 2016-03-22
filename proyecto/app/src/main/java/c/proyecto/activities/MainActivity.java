3d2347f04050c01df2eddf64a7414dec55aa458e
Alejandro Torres <alejandro123888@hotmail.com>
2016-03-22 19:04:54 +0100

    Metiendo iconos navDrawer

--- proyecto/app/src/main/java/c/proyecto/activities/MainActivity.java
+++ proyecto/app/src/main/java/c/proyecto/activities/MainActivity.java
@@ -3,36 +3,39 @@ package c.proyecto.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.ArrayList;
-
 import c.proyecto.R;
-import c.proyecto.adapters.PrestacionesAdapter;
-import c.proyecto.fragments.DetallesAnuncioFragment;
 import c.proyecto.fragments.PrincipalFragment;
 import c.proyecto.interfaces.MainActivityOps;
 import c.proyecto.models.Anuncio;
 import c.proyecto.models.Usuario;
 import c.proyecto.presenters.MainPresenter;
 
-/**
- * Created by aleja on 19/03/2016.
- */
-public class MainActivity extends AppCompatActivity implements MainActivityOps{
+public class MainActivity extends AppCompatActivity implements MainActivityOps, NavigationView.OnNavigationItemSelectedListener {
 
     private static final String ARG_USUARIO = "usuario_extra";
     private MainPresenter mPresenter;
     private Usuario user;
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         initViews();
         if (getIntent().hasExtra(ARG_USUARIO))
             user = getIntent().getParcelableExtra(ARG_USUARIO);
@@ -40,8 +43,19 @@ public class MainActivity extends AppCompatActivity implements MainActivityOps{
 
     private void initViews() {
         mPresenter = MainPresenter.getPresentador(this);
-
         getSupportFragmentManager().beginTransaction().replace(R.id.frmContenido, new PrincipalFragment()).commit();
+        configNavDrawer();
+
+    }
+
+    private void configNavDrawer() {
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        //Listener del menú del navigationDrawer
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     public static void start(Activity a, Usuario u){
@@ -128,5 +142,24 @@ public class MainActivity extends AppCompatActivity implements MainActivityOps{
     protected void onDestroy() {
         mPresenter.detachListeners();
         super.onDestroy();
+    }
+    //MENU NAV DRAWER
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                break;
+            case R.id.nav_new_adv:
+                break;
+            case R.id.nav_edit_profile:
+                break;
+            case R.id.nav_messages:
+                break;
+            case R.id.nav_preferences:
+                break;
+            case R.id.nav_sign_of:
+                break;
+        }
+        return true;
     }
 }
