d9c98a4eeaca9d362e79256f0d668229a9d633ce
Alejandro Torres <alejandro123888@hotmail.com>
2016-03-24 13:29:51 +0100

    Empezando crearAnuncio1

--- proyecto/app/src/main/java/c/proyecto/activities/CrearAnuncio1Activity.java
+++ proyecto/app/src/main/java/c/proyecto/activities/CrearAnuncio1Activity.java
@@ -3,6 +3,7 @@ package c.proyecto.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +20,12 @@ public class CrearAnuncio1Activity extends AppCompatActivity {
     private static final String INTENT_ANUNCIO = "intent anuncio1";
     private Anuncio mAnuncio;
     private ImageView imgSiguiente;
+    private ImageView imgPrincipal;
+    private ImageView img1;
+    private ImageView img2;
+    private ImageView img3;
+    private ImageView img4;
+    private ImageView img5;
 
     public static void start(Context context, @Nullable Anuncio anuncio){
         Intent intent = new Intent(context, CrearAnuncio1Activity.class);
@@ -34,11 +41,28 @@ public class CrearAnuncio1Activity extends AppCompatActivity {
     }
 
     private void initViews() {
+        imgPrincipal = (ImageView) findViewById(R.id.imgPrincipal);
+        img1 = (ImageView) findViewById(R.id.img1);
+        img2 = (ImageView) findViewById(R.id.img2);
+        img3 = (ImageView) findViewById(R.id.img3);
+        img4 = (ImageView) findViewById(R.id.img4);
+        img5 = (ImageView) findViewById(R.id.img5);
+
+
         imgSiguiente = (ImageView) findViewById(R.id.imgSiguiente);
         imgSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CrearAnuncio2Activity.start(CrearAnuncio1Activity.this, mAnuncio);
+            }
+        });
+    }
+
+    private void showImageDialog(ImageView img){
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
             }
         });
     }
---
+++ proyecto/app/src/main/java/c/proyecto/utils/Imagenes.java
@@ -0,0 +1,89 @@
+package c.proyecto.utils;
+
+
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+
+public final class Imagenes {
+
+    public static String getRealPathFromGallery(Context context, Uri galleryUri){
+        // Se consulta en el content provider de la galería el path real del archivo de la foto.
+        String[] filePath = {MediaStore.Images.Media.DATA};
+        Cursor c = context.getContentResolver().query(galleryUri, filePath, null, null, null);
+        c.moveToFirst();
+        int columnIndex = c.getColumnIndex(filePath[0]);
+        String path = c.getString(columnIndex);
+        c.close();
+        return path;
+    }
+    public static Bitmap escalar(int width, int height, String path){
+        // Se obtiene el tamaño de la imagen.
+        BitmapFactory.Options opciones = new BitmapFactory.Options();
+        opciones.inJustDecodeBounds = true; // Solo para cálculo.
+        BitmapFactory.decodeFile(path, opciones);
+        int anchoFoto = opciones.outWidth;
+        int altoFoto = opciones.outHeight;
+        // Se obtiene el factor de escalado para la imagen.
+        int factorEscalado = Math.min(anchoFoto / width,
+                altoFoto / height);
+        // Se escala la imagen con dicho factor de escalado.
+        opciones.inJustDecodeBounds = false; // Se escalará.
+        opciones.inSampleSize = factorEscalado;
+        return BitmapFactory.decodeFile(path, opciones);
+    }
+
+    public static boolean guardarBitmapEnArchivo(Bitmap bitmapFoto, File archivo) {
+        try {
+            FileOutputStream flujoSalida = new FileOutputStream(archivo);
+            bitmapFoto.compress(Bitmap.CompressFormat.JPEG, 100, flujoSalida);
+            flujoSalida.flush();
+            flujoSalida.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Crea un archivo de foto con el nombre indicado en almacenamiento externo si es posible, o si
+    // no en almacenamiento interno, y lo retorna. Retorna null si fallo.
+    // Si publico es true -> en la carpeta pública de imágenes.
+    // Si publico es false, en la carpeta propia de imágenes.
+    private File crearArchivoFoto(Context context, String nombre, boolean publico) {
+        // Se obtiene el directorio en el que almacenarlo.
+        File directorio;
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            if (publico)
+                // En el directorio público para imágenes del almacenamiento externo.
+                directorio = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            else
+                directorio = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        } else {
+            // En almacenamiento interno.
+            directorio = context.getFilesDir();
+        }
+        // Su no existe el directorio, se crea.
+        if (directorio != null && !directorio.exists())
+            if (!directorio.mkdirs())
+                return null;
+
+        // Se crea un archivo con ese nombre y la extensión jpg en ese
+        // directorio.
+        File archivo = null;
+        if (directorio != null)
+            archivo = new File(directorio.getPath() + File.separator + nombre);
+
+        // Se retorna el archivo creado.
+        return archivo;
+    }
+
+}
