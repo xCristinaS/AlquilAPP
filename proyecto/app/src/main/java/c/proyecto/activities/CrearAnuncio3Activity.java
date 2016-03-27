2a0abcb80541fd83ca4b4a10d0721d579e6d936c
xCristinaS <cristina.sola.1992@gmail.com>
2016-03-27 17:04:26 +0200

    empezando huespedes

---
+++ proyecto/app/src/main/java/c/proyecto/activities/CrearAnuncio3Activity.java
@@ -0,0 +1,15 @@
+package c.proyecto.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import c.proyecto.R;
+
+public class CrearAnuncio3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_anuncio3);
+    }
+}
