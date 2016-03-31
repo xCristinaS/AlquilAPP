fdf7a4520a8cff4e9cc980245f06c262f464d642
Alejandro Torres <alejandro123888@hotmail.com>
2016-03-31 21:34:47 +0200

    commit inicial

---
+++ proyecto/app/src/main/java/c/proyecto/activities/VerPerfilActivity.java
@@ -0,0 +1,12 @@
+package c.proyecto.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
