2fd0abedd61f4db5b82d5ef2d14d389624eaf26d
xCristinaS <cristina.sola.1992@gmail.com>
2016-05-21 17:01:09 +0200

    configuración firebase cloud messaging hecha

---
+++ proyecto/app/src/main/java/c/proyecto/notificaciones_fcm/MyFirebaseInstanceIDService.java
@@ -0,0 +1,18 @@
+package c.proyecto.notificaciones_fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("Refreshed token: ", refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
---
+++ proyecto/app/src/main/java/c/proyecto/notificaciones_fcm/MyFirebaseMessagingService.java
@@ -0,0 +1,12 @@
+package c.proyecto.notificaciones_fcm;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService  extends FirebaseMessagingService{
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
