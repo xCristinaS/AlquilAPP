7562eed65003a09f33f9faad695da4370b57d1bd
Alejandro Torres <alejandro123888@hotmail.com>
2016-06-04 15:54:23 +0200

    Todas las actividades obligadas a PORTRAIT

--- proyecto/app/src/main/java/c/proyecto/App.java
+++ proyecto/app/src/main/java/c/proyecto/App.java
@@ -1,6 +1,9 @@
 package c.proyecto;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
 import com.facebook.stetho.Stetho;
 import com.firebase.client.Firebase;
@@ -12,5 +15,31 @@ public class App extends Application {
         Firebase.setAndroidContext(this);
         Stetho.initializeWithDefaults(this);
         super.onCreate();
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                //Obliga a todas las actividades creadas a mostrarse solo en PORTRAIT
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {}
+
+            @Override
+            public void onActivityResumed(Activity activity) {}
+
+            @Override
+            public void onActivityPaused(Activity activity) {}
+
+            @Override
+            public void onActivityStopped(Activity activity) {}
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {}
+        });
     }
 }
