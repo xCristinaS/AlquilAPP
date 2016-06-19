f1ab4e47a60784901f69fe8571ddaf296a665c80
Alejandro Torres <alejandro123888@hotmail.com>
2016-06-19 16:18:01 +0200

    progressBar cuando en el splash se tarda mucho en iniciar sesión debido a una mala conexión

--- proyecto/app/src/main/java/c/proyecto/activities/SplashActivity.java
+++ proyecto/app/src/main/java/c/proyecto/activities/SplashActivity.java
@@ -2,11 +2,16 @@ package c.proyecto.activities;
 
 import android.animation.Animator;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import c.proyecto.Constantes;
 import c.proyecto.R;
@@ -21,6 +26,7 @@ public class SplashActivity extends AppCompatActivity implements MyInicio {
     private InicioPresenter mPresenter;
     private Usuario mUser;
     private Lanzador hiloLanzador;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,8 @@ public class SplashActivity extends AppCompatActivity implements MyInicio {
 
     private void initView() {
         imgLogo = (ImageView) findViewById(R.id.imgLogo);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.getIndeterminateDrawable().setColorFilter(getResources().getColor(R.color.colorAccent), android.graphics.PorterDuff.Mode.SRC_IN);
 
         imgLogo.animate().scaleX(1.3f).scaleY(1.3f).setDuration(1400).setListener(new Animator.AnimatorListener() {
             @Override
@@ -45,6 +53,7 @@ public class SplashActivity extends AppCompatActivity implements MyInicio {
 
             @Override
             public void onAnimationEnd(Animator animation) {
+
                 imgLogo.animate().scaleX(1).scaleY(1).setDuration(1000).setListener(new Animator.AnimatorListener() {
                     @Override
                     public void onAnimationStart(Animator animation) {
@@ -54,6 +63,7 @@ public class SplashActivity extends AppCompatActivity implements MyInicio {
                     @Override
                     public void onAnimationEnd(Animator animation) {
                         hiloLanzador.countDown.countDown();
+                        new HiloProgresBar().execute();
                     }
 
                     @Override
@@ -122,6 +132,24 @@ public class SplashActivity extends AppCompatActivity implements MyInicio {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    class HiloProgresBar extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(4500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            progressBar.setVisibility(View.VISIBLE);
         }
     }
 }
